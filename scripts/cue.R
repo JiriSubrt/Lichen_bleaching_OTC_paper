40391a3d465219f502cb7f84a6f282462201ea88
“JiriSubrt” <s1863483@ed.ac.uk>
2025-09-17 18:33:06 +0100

    Adding all files for publication

---
+++ scripts/cue.R
@@ -0,0 +1,55 @@
+# CUE script
+# Jiri Subrt
+# 28/07/2023
+
+library(tidyverse)
+
+# Getting max NP values, and corresponsing DR values
+cuedata <- read.csv("data/cue/np_dr_gp_corresponding.csv")
+
+# Set values that are negative to 0, because CUE cannot be zero
+cue_capped <- cuedata %>% 
+  mutate(np_dw = np_dw/1000) %>% 
+  pivot_wider(names_from = measurement, values_from = np_dw) %>% 
+  group_by(sample, temperature, treatment_type) %>% 
+  mutate(CUE = NP / GP * 100,
+         CUE = ifelse(CUE < 0, 0, CUE)) %>%  # Set negative values of CUE to 0
+  dplyr::select(!c(GP, DR, NP)) 
+
+cue_capped$temperature <- as.factor(cue_capped$temperature)
+cue_capped$treatment_type <- as.factor(cue_capped$treatment_type)
+
+colours <- c("dark blue", "red")
+
+
+figure_5 <- ggplot(cue_capped, aes(x = temperature, y = CUE, fill = treatment_type, color = treatment_type)) +
+  
+  # Add means with SEM as points with error bars (aligned, not dodged)
+  stat_summary(fun.data = function(x) {
+    data.frame(
+      y = mean(x),
+      ymin = mean(x) - sd(x) / sqrt(length(x)),
+      ymax = mean(x) + sd(x) / sqrt(length(x))
+    )
+  }, geom = "pointrange", size = 1.5, alpha = 0.75, position = position_identity()) +
+  
+  # Increase point size
+  stat_summary(fun = mean, geom = "point", size = 4, alpha = 0.75, position = position_identity()) +
+  
+  # Set colors manually
+  scale_color_manual(values = colours) +
+  
+  # Optional: set fill too if using shapes that include fill
+  scale_fill_manual(values = colours) +
+  
+  # Other styling options
+  ylab("Carbon use efficiency (%)") +
+  xlab("Temperature (˚C)") +
+  theme_bw(base_size = 15) +
+  theme(legend.position = "none", legend.title = element_blank(), legend.text = element_text(size = 12))
+
+
+
+
+ggsave("outcomes/cue/figure_5.png", plot = figure_5, width = 10, height = 5, dpi = 300)
+
